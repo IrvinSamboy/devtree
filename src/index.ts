a300c3ff7292d5d4d9d4444742a55ea601391992
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-11 23:33:00 -0400

    Connecting database

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from "mongoose";
 
 dotenv.config()
 
 const app = express()
 
 const PORT : number = Number.parseInt(process.env.PORT!)
+const connectionString : string = process.env.MONGOURL || ''
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+export const startServer = async () : Promise<void> => {
+    try{
+        await mongoose.connect(connectionString)
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    }
+    catch(err ){
+        console.log(err)
+    }
+}
+
+startServer()
