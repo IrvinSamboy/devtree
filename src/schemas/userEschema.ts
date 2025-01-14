64451ee8db89f9906bcc8643c0ea1fb13ba5b7cf
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-13 23:13:12 -0400

    fixing schema issues

--- src/schemas/userEschema.ts
+++ src/schemas/userEschema.ts
@@ -1,5 +1,22 @@
 import { z } from 'zod'
 
+export enum StatusCode {
+    OK = 200,
+    Created = 201,
+    NoContent = 204,
+    BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
+    NotFound = 404,
+    MethodNotAllowed = 405,
+    InternalServerError = 500,
+    NotImplemented = 501,
+    BadGateway = 502,
+    ServiceUnavailable = 503,
+    GatewayTimeout = 504
+}
+
+
 export const SignupEschema = z.object({
     username: z.string(),
     email: z.string(),
@@ -7,6 +24,7 @@ export const SignupEschema = z.object({
 })
 
 export const SignupEschemaResponse = z.object({
+    status: z.nativeEnum(StatusCode),
     body: SignupEschema
 })
 
