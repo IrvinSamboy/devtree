afdd8f5a6c1039a28a083754ecd3f8387a87e670
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-20 19:21:49 -0400

    Creating schema validation to avoid empty values

--- src/schemas/userEschema.ts
+++ src/schemas/userEschema.ts
@@ -6,10 +6,10 @@ export const Message = z.object({
 })
 
 export const ZUserEschema = z.object({
-    userName: z.string(),
-    name: z.string(),
-    email: z.string(),
-    password: z.string()
+    userName: z.string().min(4, {message: "User name must have at least 4 characters"}),
+    name: z.string().min(4, {message: "Name must have at least 4 characters"}),
+    email: z.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {message: "Invalid email address"}),
+    password: z.string().nonempty({message: "Password is required"})
 })
 
 export const SignupEschemaResponse = z.object({
