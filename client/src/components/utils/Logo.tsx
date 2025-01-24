8b2d1e84df1ce7f11f76dae72c239abc3e3a913c
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-24 17:16:53 -0400

    little changes in header component

--- client/src/components/utils/Logo.tsx
+++ client/src/components/utils/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <div className="flex gap-3 text-mid-purple">
             <Code2 />
-            <p className="font-semibold text-xl">DEVTREE</p>
+            <p className="font-bold text-xl">DevTree</p>
         </div>
     )
 }
