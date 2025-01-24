0f64e96d645a30dd84b6f680a3582ed8c9823ca2
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-24 11:40:17 -0400

    Creating rootlayout

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AuthPage from "../pages/auth/Auth.page"
+import RootLayout from "../components/layout/RootLayout"
 
 export default function Router() {
   return (
     <BrowserRouter >
         <Routes>
             <Route
-                path="/auth/"
-                element={<AuthPage />}
-            />
+              element={<RootLayout />}
+            >
+              <Route
+                  path="/auth/"
+                  element={<AuthPage />}
+              />
+            </Route>
         </Routes>
     </BrowserRouter>
   )
