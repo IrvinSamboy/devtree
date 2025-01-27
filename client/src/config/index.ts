59fdd962d56df4bc8954adde12db896c5d974f4d
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-27 16:15:26 -0400

    Creating axios config to make request

---
+++ client/src/config/index.ts
@@ -0,0 +1,2 @@
+
+export const apiURL = import.meta.env.APIURL 
---
+++ client/src/providers/apliClient.ts
@@ -0,0 +1,7 @@
+import { apiURL } from '../config'
+import axios from 'axios'
+
+export const apiDevTree = axios.create({
+    baseURL: apiURL,
+    timeout: 6000
+})
