01abbf584711cf3d4c81ad1cc88afa6aab9d0979
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-01-28 16:41:51 -0400

    Creating signup provider

--- client/src/providers/Auth/index.ts
+++ client/src/providers/Auth/index.ts
@@ -1,13 +1,29 @@
 import { apiDevTreeEndPoints } from "../endPoints";
 import { apiDevTree } from "../apliClient";
 import { useMutation } from 'react-query'
-import { signInRequestT, signInResponseI, ErrorMessage } from "./auth.interfaces";
+import { signInRequestT, signInResponseI, ErrorMessage, signUpRequestT, signUpResponseT } from "./auth.interfaces";
 
 export const useSignin = () => {
     return useMutation<signInResponseI, ErrorMessage, signInRequestT>({
         mutationKey: ['signin'],
         mutationFn: async (data) => {
             const response = await apiDevTree.post(apiDevTreeEndPoints.auth.signin, {
+                email: data.email,
+                password: data.password
+            })
+
+            return response.data
+        }
+    })
+}
+
+export const useSignUp = () => {
+    return useMutation<signUpResponseT, ErrorMessage, signUpRequestT>({
+        mutationKey: ['signup'],
+        mutationFn: async (data) => {
+            const response = await apiDevTree.post(apiDevTreeEndPoints.auth.signup, {
+                name: data.name,
+                username: data.username,
                 email: data.email,
                 password: data.password
             })
