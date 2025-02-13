8a29b674f8b9c3e4e3b97b69ec645dccbff61af2
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-13 15:12:41 -0400

    Creating verify session token middleware

---
+++ server/src/middlewares/verifySessionToken.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express"
+import { verifyToken } from "../utils/handleJWT"
+
+export const verifySessionToken = (req : Request, res : Response, next : NextFunction) => {
+    try {
+        const {devtreeToken} = req.cookies
+        
+        if(!devtreeToken) return res.status(403).json({message: 'You need to be authenticated'})
+
+        const tokenVerified = verifyToken(devtreeToken)
+
+        if(!tokenVerified) return res.status(403).json({message: 'Invalid sessionToken'})
+        
+
+        next()
+    }
+    catch  (e) {
+        const errorMessage = (e as Error).message
+        
+        return res.status(500).json({message: errorMessage})
+    }
+} 
