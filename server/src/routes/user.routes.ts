bfe62dd5ebff7b47e3a25c823009c9c912bec4be
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-13 16:55:07 -0400

    Adding middleware

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,12 +1,18 @@
 import { initServer } from "@ts-rest/express";
 import { userContract } from "../contracts/userContract";
 import { signup, signin, userData } from "../controllers/user.controllers";
+import { verifySessionToken } from "../middlewares/verifySessionToken";
 
 const s = initServer()
 
 export const userRoutes = s.router(userContract, {
     signup : signup,
     signin: signin,
-    userData: userData
+    userData: {
+        middleware: [
+            verifySessionToken
+        ],
+        handler: userData
+    }
 })
 
