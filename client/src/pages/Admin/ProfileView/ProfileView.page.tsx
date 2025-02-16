d965b4d40f0210100762c84af64da54177129689
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-15 18:24:47 -0800

    Adding inputs

--- client/src/pages/Admin/ProfileView/ProfileView.page.tsx
+++ client/src/pages/Admin/ProfileView/ProfileView.page.tsx
@@ -1,7 +1,21 @@
+import Input from "../../../components/ui/Input"
+
 export default function ProfileView() {
   return (
     <div className="bg-white p-4">
       <h2 className="text-center text-xl font-semibold">Edit information</h2>
+      <div className="space-y-2">
+        <Input
+            placeHolder="User name"
+            label = "User name"
+            type = "text"
+        />
+        <Input
+            placeHolder="email"
+            label = "Email"
+            type = "text"
+        />
+      </div>
     </div>
   )
 }
