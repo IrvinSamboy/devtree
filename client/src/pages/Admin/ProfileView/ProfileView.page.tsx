fdfdfc640014cec793dba0b0684d37c8d2d3e3da
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-15 18:20:30 -0800

    getting started with profile view commponent

--- client/src/pages/Admin/ProfileView/ProfileView.page.tsx
+++ client/src/pages/Admin/ProfileView/ProfileView.page.tsx
@@ -1,7 +1,7 @@
 export default function ProfileView() {
   return (
-    <div>
-      <p>ProfileView</p>
+    <div className="bg-white p-4">
+      <h2 className="text-center text-xl font-semibold">Edit information</h2>
     </div>
   )
 }
