9e61fca83a72e632c7e3e0c1170e6b59a30a690a
IrvinSamboy <contacoirvinsamboy@gmail.com>
2025-02-16 22:00:24 -0400

    Creating design for the drag and drop image

--- client/src/pages/Admin/ProfileView/ProfileView.page.tsx
+++ client/src/pages/Admin/ProfileView/ProfileView.page.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react"
 import Input from "../../../components/ui/Input"
 
 export default function ProfileView() {
+
+  const [drag, setDrag] = useState(false)
   
   const handleOnDrop = (e : React.DragEvent<HTMLDivElement>) => {
     e.preventDefault()
     console.log("drop")
   }
-  const handleOnDras = (e : React.DragEvent<HTMLDivElement>) => {
+  const handleOnDragOver = (e : React.DragEvent<HTMLDivElement>) => {
     e.preventDefault()
-    console.log("drag")
+    setDrag(true)
+  }
+  const handleOnDragEnd = (e : React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setDrag(false)
   }
   return (
     <div className="bg-white p-4 rounded-lg shadow-lg">
@@ -24,9 +31,12 @@ export default function ProfileView() {
             label = "Email"
             type = "text"
         />
-        <div onDragOver={handleOnDras} onDrop={handleOnDrop}>
-          <p>hi</p>
-        </div>
+       <div className="space-y-1">
+          <p>Image</p>
+          <div className={`py-10 px-4 border-mid-purple border rounded-lg ${drag&& 'bg-mid-purple/50'}`} onDragLeave={handleOnDragEnd} onDragOver={handleOnDragOver} onDrop={handleOnDrop}>
+            <p className="text-center text-xl text-gray-500">Drag a image here to upload or <br/><span className="text-lg border-b ">CLick here to browse</span></p>   
+          </div>
+       </div>
       </div>
     </div>
   )
