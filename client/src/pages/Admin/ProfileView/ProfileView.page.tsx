844a3645f1976cab5c6c8ca553d7595d67b72900
IrvinSamboy <contacoirvinsamboy@gmail.com>
2025-02-16 21:05:43 -0400

    getting started with drag and drop images input

--- client/src/pages/Admin/ProfileView/ProfileView.page.tsx
+++ client/src/pages/Admin/ProfileView/ProfileView.page.tsx
@@ -1,8 +1,17 @@
 import Input from "../../../components/ui/Input"
 
 export default function ProfileView() {
+  
+  const handleOnDrop = (e : React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    console.log("drop")
+  }
+  const handleOnDras = (e : React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    console.log("drag")
+  }
   return (
-    <div className="bg-white p-4">
+    <div className="bg-white p-4 rounded-lg shadow-lg">
       <h2 className="text-center text-xl font-semibold">Edit information</h2>
       <div className="space-y-2">
         <Input
@@ -15,6 +24,9 @@ export default function ProfileView() {
             label = "Email"
             type = "text"
         />
+        <div onDragOver={handleOnDras} onDrop={handleOnDrop}>
+          <p>hi</p>
+        </div>
       </div>
     </div>
   )
