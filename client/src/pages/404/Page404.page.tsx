5d0a46f8855b0e57ca92476555db8d2d931f9dc8
Irvin SAmboy <irvinsamboy@gmail.com>
2025-02-20 16:52:22 -0400

    404 page maketing

---
+++ client/src/pages/404/Page404.page.tsx
@@ -0,0 +1,13 @@
+
+export default function Page404() {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <div>
+        <h1>404</h1>
+        <h2>PAge Not FOund</h2>
+        <p>Oops! The page you're looking for doesn't exist.</p>
+        <button>Return Home</button>
+      </div>
+    </div>
+  )
+}
