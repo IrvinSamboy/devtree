0cee3c6b77a7bc58d37bb950809ee0f7a385ef7a
IrvinSamboy <contacoirvinsamboy@gmail.com>
2025-02-21 21:18:24 -0400

    Same fix in user data

--- client/src/providers/User/index.ts
+++ client/src/providers/User/index.ts
@@ -2,9 +2,10 @@ import { apiDevTreeEndPoints } from "../endPoints";
 import { useQuery } from "react-query";
 import { userDataResponseT, MessageT } from "./user.interface";
 import { apiDevTree } from "../apiClient";
+import { AxiosError } from "axios";
 
 export const useUserData = () => {
-    return useQuery<userDataResponseT, MessageT>({
+    return useQuery<userDataResponseT, AxiosError<MessageT>>({
         queryKey: ["userData"],
         queryFn: async () => {
             const response = await apiDevTree.get(apiDevTreeEndPoints.user.userData, {withCredentials: true})
