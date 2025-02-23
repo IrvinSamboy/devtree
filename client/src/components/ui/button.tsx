07a1a3b8fd6cbff1286818d9f60609750371e3db
IrvinSamboy <contacoirvinsamboy@gmail.com>
2025-02-23 17:55:48 -0400

    creatinh button component

---
+++ client/src/components/ui/button.tsx
@@ -0,0 +1,18 @@
+import Loader from "../utils/Loader";
+
+type buttonComponentPropsT = {
+    isLoading : boolean;
+    message : string;
+    styles : string;
+}
+
+export default function button({isLoading, message, styles} : buttonComponentPropsT) {
+    <button
+        disabled={isLoading}
+        className={`w-full text-center p-2 bg-mid-purple font-bold text-white rounded-lg
+         cursor-pointer ${styles}`}
+    >
+        {isLoading ? <Loader /> : message}
+    </button>
+  )
+}
