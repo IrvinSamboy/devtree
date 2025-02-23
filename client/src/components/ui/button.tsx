d9057be810f253ade5cadeac685bf82ca65e7675
IrvinSamboy <contacoirvinsamboy@gmail.com>
2025-02-23 17:57:56 -0400

    modifying loader component to add styles

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -7,6 +7,7 @@ type buttonComponentPropsT = {
 }
 
 export default function button({isLoading, message, styles} : buttonComponentPropsT) {
+    return (
     <button
         disabled={isLoading}
         className={`w-full text-center p-2 bg-mid-purple font-bold text-white rounded-lg
--- client/src/components/utils/Loader.tsx
+++ client/src/components/utils/Loader.tsx
@@ -1,7 +1,8 @@
-export default function Loader() {
+export default function Loader({styles} : {styles : string}) {
   return (
     <div className="flex h-full items-center justify-center">
-        <div className="border-4 rounded-full border-mid-purple p-10 border-b-trasparent border-r-transparent animate-spin"></div>
+        <div className={`border-4 rounded-full border-mid-purple p-10 border-b-trasparent
+         border-r-transparent animate-spin ${styles}`}></div>
     </div>
   )
 }
