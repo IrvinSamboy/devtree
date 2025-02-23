9bb35fe6b3fdf491d0997af762f99e974550d66c
IrvinSamboy <contacoirvinsamboy@gmail.com>
2025-02-22 22:49:32 -0400

    Creating verify user session token contract

--- server/src/contracts/userContract.ts
+++ server/src/contracts/userContract.ts
@@ -38,6 +38,17 @@ export const userContract = c.router({
             500: Message
         },
         summary: 'get user data'
+    },
+
+    verifyUserSession: {
+        method: 'GET',
+        path: '/verifyUSerSession',
+        responses: {
+            200: Message,
+            401: Message,
+            500 : Message
+        },
+        summary: 'verify if user is logged in with his token'
     }
 
 })
