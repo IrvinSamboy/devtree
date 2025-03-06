746ae30398e0261ae4b5486c3c61b4fdd89aa9cc
IrvinSamboy <contacoirvinsamboy@gmail.com>
2025-03-05 21:43:05 -0400

    getting started with social media component

--- client/src/interfaces/User.interface.ts
+++ client/src/interfaces/User.interface.ts
@@ -10,3 +10,12 @@ export interface UserI {
 export type InputsSigninT = Omit<UserI, "username" | "name"> 
 
 export type InputsSignupT = UserI
+
+export type socialMedia = {
+    id: number,
+    name: string,
+    url: string,
+    enabled: boolean
+}
+
+export type devTreeLink = Omit<socialMedia, 'id'>
--- client/src/pages/Admin/DevTreeView/DevTreeView.page.tsx
+++ client/src/pages/Admin/DevTreeView/DevTreeView.page.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
+import { social } from "../../../data/social"
+import { devTreeLink } from "../../../interfaces/User.interface"
+
 export default function DevTreeView() {
+  
+  const [socialMediaLink, setSocialMediaLink] = useState<devTreeLink>(social)
+
   return (
     <div>
         <p>DevTreeView</p>      
